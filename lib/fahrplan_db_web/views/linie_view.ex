163d12aae6c49470e46c1c8fc647ee853330627b
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-09 11:33:19 +0200

    add helper functions in LinieView

--- lib/fahrplan_db_web/views/linie_view.ex
+++ lib/fahrplan_db_web/views/linie_view.ex
@@ -1,3 +1,17 @@
 defmodule FahrplanDbWeb.LinieView do
   use FahrplanDbWeb, :view
+
+  def haltestellen_name_id(haltestellen) do
+    Enum.map(haltestellen, fn x -> {x.name, x.id} end)
+  end
+
+  def linie_has_haltestelle?(linie, haltestelle)do
+    case Enumerable.impl_for(linie.haltestellen) do
+      nil -> false
+      _ ->
+        Enum.map(linie.haltestellen, fn x -> x.id end)
+        |>Enum.member?(haltestelle.id)
+
+    end
+  end
 end

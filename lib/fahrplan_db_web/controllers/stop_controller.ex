6b2ce28e12db27321bc1b2fb80608af1cd70b3d2
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-15 14:04:34 +0200

    add function for the 'create' action

--- lib/fahrplan_db_web/controllers/stop_controller.ex
+++ lib/fahrplan_db_web/controllers/stop_controller.ex
@@ -15,4 +15,34 @@ defmodule FahrplanDbWeb.StopController do
     linien = Fahrplan.list_linien()
     render(conn, "new.html", changeset: changeset, haltestellen: haltestellen, linien: linien)
   end
+
+  def create(conn, %{"stop" => stop_params}) do
+    updated_params = create_params_with_structs(stop_params)
+
+    case Fahrplan.create_stop(updated_params) do
+      {:ok, _stop} ->
+        conn
+        |> put_flash(:info, "stop created successfully.")
+        |> redirect(to: Routes.stop_path(conn, :index, Fahrplan.list_stops()))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        haltestellen = Fahrplan.list_haltestellen()
+        linien = Fahrplan.list_linien()
+        render(conn, "new.html", changeset: changeset, haltestellen: haltestellen, linien: linien)
+    end
+  end
+
+  def create_params_with_structs(
+        %{"haltestelle_id" => haltestelle_id, "linie_id" => linie_id} = stop_params
+      ) do
+    structs = %{
+      "haltestelle" => Fahrplan.get_haltestelle_preload_linien(haltestelle_id),
+      "linie" => Fahrplan.get_linie(linie_id)
+    }
+
+    stop_params
+    |> Map.delete("haltestelle_id")
+    |> Map.delete("linie_id")
+    |> Map.merge(structs)
+  end
 end

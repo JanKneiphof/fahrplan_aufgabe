828d33b24f8ea8d3c69fe24c7fdc1672a5a4b299
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-15 14:01:02 +0200

    add changeset function for 'stop' Schema

--- lib/fahrplan_db/fahrplan/stop.ex
+++ lib/fahrplan_db/fahrplan/stop.ex
@@ -1,6 +1,6 @@
 defmodule FahrplanDb.Stop do
   use Ecto.Schema
-  import Ecto.Schema
+  import Ecto.Changeset
 
   schema "stop" do
     belongs_to(:haltestelle, FahrplanDb.Haltestelle)
@@ -8,4 +8,23 @@ defmodule FahrplanDb.Stop do
     field(:uhrzeit, :time)
   end
 
+  def changeset(stop, attrs) do
+    stop
+    |> cast(attrs, [:uhrzeit])
+    |> put_assoc(:linie, attrs["linie"])
+    |> put_assoc(:haltestelle, attrs["haltestelle"])
+    |> validate_required([:haltestelle, :linie, :uhrzeit])
+    # |> validate_combination_allowed(attrs["haltestelle"])
+    # Die auskommentierte Validation lehnt alles ab, irgendwas stimmt damit nicht
+  end
+
+  def validate_combination_allowed(changeset, nil) do
+    validate_inclusion(changeset, :linie, [])
+  end
+
+  def validate_combination_allowed(changeset, haltestelle) do
+    validate_inclusion(changeset, :linie, haltestelle.linien,
+      message: "Die gewählte Linie fährt die gewählte Haltestelle nicht an."
+    )
+  end
 end

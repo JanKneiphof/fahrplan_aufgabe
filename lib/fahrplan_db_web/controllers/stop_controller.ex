cf81c1d8c742801fc3e2ec7f15942a852e94a3b0
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-15 13:47:37 +0200

    add controller method for action 'new'

--- lib/fahrplan_db_web/controllers/stop_controller.ex
+++ lib/fahrplan_db_web/controllers/stop_controller.ex
@@ -2,8 +2,17 @@ defmodule FahrplanDbWeb.StopController do
   use FahrplanDbWeb, :controller
 
   alias FahrplanDb.Fahrplan
+  alias FahrplanDb.Stop
+
   def show(conn, %{"id" => id}) do
     stop = Fahrplan.get_stop!(id)
     render(conn, "show.html", stop: stop)
+  end
+
+  def new(conn, _params) do
+    changeset = Fahrplan.change_stop(%Stop{})
+    haltestellen = Fahrplan.list_haltestellen()
+    linien = Fahrplan.list_linien()
+    render(conn, "new.html", changeset: changeset, haltestellen: haltestellen, linien: linien)
   end
 end

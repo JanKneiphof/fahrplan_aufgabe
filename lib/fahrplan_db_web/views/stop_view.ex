eb604e091d4c2d2115278149272a15f60790c0d9
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-15 14:03:58 +0200

    add helper functions for the 'form' template

--- lib/fahrplan_db_web/views/stop_view.ex
+++ lib/fahrplan_db_web/views/stop_view.ex
@@ -1,3 +1,13 @@
 defmodule FahrplanDbWeb.StopView do
   use FahrplanDbWeb, :view
+
+  def haltestellen_select_options(haltestellen) do
+    id_name_tuple_list = Enum.map(haltestellen, fn x -> {x.name, x.id} end)
+    [{"Bitte Haltestelle wählen", -1}] ++ id_name_tuple_list
+  end
+
+  def linien_select_options(linien) do
+    id_name_tuple_list = Enum.map(linien, fn x -> {x.name, x.id} end)
+    [{"Bitte Linie wählen", -1}] ++ id_name_tuple_list
+  end
 end

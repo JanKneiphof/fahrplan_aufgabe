d0efc5d4e22cce990c7e68cfa432a14dd0097525
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-16 13:47:32 +0200

    refactor 'linie_has_haltestelle?(...)'

--- lib/fahrplan_db_web/views/linie_view.ex
+++ lib/fahrplan_db_web/views/linie_view.ex
@@ -6,7 +6,7 @@ defmodule FahrplanDbWeb.LinieView do
   end
 
   def linie_has_haltestelle?(linie, haltestelle) do
-    case Enumerable.impl_for(linie.haltestellen) do
+    case linie do
       nil ->
         false
 
@@ -14,5 +14,9 @@ defmodule FahrplanDbWeb.LinieView do
         Enum.map(linie.haltestellen, fn x -> x.id end)
         |> Enum.member?(haltestelle.id)
     end
+  end
+
+  def render("new.html", assigns) do
+    render_template("new.html", Map.merge(assigns, %{linie: nil}))
   end
 end

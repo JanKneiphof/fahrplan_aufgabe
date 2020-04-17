951e72c6056d7aabb147cdf098c0fcc5ef8c3c9b
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-17 12:42:22 +0200

    correct migration file

--- priv/repo/migrations/20200416070031_update_delete_behavior_on_stop.exs
+++ priv/repo/migrations/20200416070031_update_delete_behavior_on_stop.exs
@@ -3,8 +3,10 @@ defmodule FahrplanDb.Repo.Migrations.UpdateDeleteBehaviorOnStop do
 
   def change do
     alter table("stop") do
-      modify(:haltestelle_id, references(:haltestelle, on_delete: :delete_all))
-      modify(:linie_id, references(:linie, on_delete: :delete_all))
+      remove(:haltestelle_id)
+      remove(:linie_id)
+      add(:haltestelle_id, references(:haltestelle, on_delete: :delete_all))
+      add(:linie_id, references(:linie, on_delete: :delete_all))
     end
   end
 end

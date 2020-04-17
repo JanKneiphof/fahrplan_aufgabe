24af564cdc1b3bfbae675f6216529ecc20bec233
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2020-04-17 13:52:45 +0200

    update validation

--- lib/fahrplan_db/fahrplan/stop.ex
+++ lib/fahrplan_db/fahrplan/stop.ex
@@ -11,22 +11,23 @@ defmodule FahrplanDb.Stop do
   def changeset(stop, attrs) do
     stop
     |> cast(attrs, [:uhrzeit])
-    |> put_assoc(:linie, attrs["linie"])
     |> put_change_id(:linie_id, attrs["linie"])
-    |> put_assoc(:haltestelle, attrs["haltestelle"])
     |> put_change_id(:haltestelle_id, attrs["haltestelle"])
-    |> validate_combination_allowed(attrs["haltestelle"])
+    |> validate_combination_allowed(attrs["haltestelle"], attrs["linie"])
     |> validate_required([:haltestelle, :linie, :uhrzeit])
 
   end
 
-  def validate_combination_allowed(changeset, nil) do
+  def validate_combination_allowed(changeset, nil, nil) do
     validate_inclusion(changeset, :linie_id, [])
   end
 
-  def validate_combination_allowed(changeset, haltestelle) do
+  def validate_combination_allowed(changeset, haltestelle, linie) do
     validate_inclusion(changeset, :linie_id, Enum.map(haltestelle.linien, fn x -> x.id end),
       message: "Die gewählte Linie fährt die gewählte Haltestelle nicht an."
+    )
+    validate_inclusion(changeset, :haltestelle_id, Enum.map(linie.haltestellen, fn x -> x.id end),
+      message: "Die gewählte Haltestelle wird von der gewählten Linie nicht angefahren."
     )
   end
 
